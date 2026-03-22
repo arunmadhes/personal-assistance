3dd805114cd9147f5c04a8b1d1e34bc5426e094b
arunmadhes <arunmadhes@gmail.com>
2026-03-22 14:53:52 +0530

    update

--- analyzer/indiaquant_web/static/sw.js
+++
@@ -1,7 +0,0 @@
-const CACHE = 'iq-v1';
-const ASSETS = ['/', '/static/icons/icon-192.png'];
-self.addEventListener('install', e => e.waitUntil(caches.open(CACHE).then(c => c.addAll(ASSETS))));
-self.addEventListener('fetch', e => {
-  if (e.request.url.includes('/api/')) return;
-  e.respondWith(fetch(e.request).catch(() => caches.match(e.request)));
-});
